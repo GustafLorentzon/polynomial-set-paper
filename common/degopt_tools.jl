bc571f39a1b01c0d06ba2ae7107986499a180925
GustafLorentzon <lorentzongustaf@gmail.com>
2025-03-24 09:21:35 +0100

    split structure imposing functions into seperate file structure_gen.jl

--- common/degopt_tools.jl
+++ common/degopt_tools.jl
@@ -1,4 +1,4 @@
 include("pretty_print.jl")
 include("degopt_ops.jl")
 #include("gustaf_helpers.jl")
-include("../equiv_trans_2025/equivalence_transformations2025.jl")
+include("../equiv_trans_2025/structure_gen.jl")
--- equiv_trans_2025/equivalence_transformations2025.jl
+++ equiv_trans_2025/equivalence_transformations2025.jl
@@ -226,28 +226,3 @@ function structure_check(HA,HB)
     Y = maximum([col1check, normcheck, b22check])
     return Y
 end
-
-
-function normalize_col1(Ha,Hb,c)
-    m=size(Ha,1);
-    for i=1:m
-        (Ha,Hb,c)=col1shift(Ha, Hb, c, i, -Ha[i,1])
-        (Hb,Ha,c)=col1shift(Hb, Ha, c, i, -Hb[i,1])
-    end
-    return (Ha,Hb,c);
-end
-
-function normalize_superdiags(Ha,Hb,c)
-    m=size(Ha,1);
-    for i=1:m
-        j=findlast(Ha[i,:] .!= 0);
-        (Ha,Hb,c)=rowscaling(Ha, Hb, c, i, 1/Ha[i,j])
-        j=findlast(Hb[i,:] .!= 0);
-        (Hb,Ha,c)=rowscaling(Hb, Ha, c, i, 1/Hb[i,j])
-    end
-    return (Ha,Hb,c);
-end
-
-function normalize_ab22(Ha,Hb,c)
-    shift_ab22(Ha, Hb, c, -Ha[2,2])
-end
